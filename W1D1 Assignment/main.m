3b2f11f8cfda86e53a0dde9d6459ea5e3ed4a639
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-06-28 11:51:14 -0700

    W1D1

--- W1D1 Assignment/main.m
+++ W1D1 Assignment/main.m
@@ -10,8 +10,123 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        
+        // UNCOMMENT THE CODES TO SEE RESULTS
+        
+        
+        
+        // Make it UPPERCASE:
+
+//        char randomText[225];
+//        
+//        NSLog(@"Enter whatever and make it LOUDER!");
+//        
+//        fgets(randomText, 225, stdin);
+//        
+//        NSLog(@"The text you entered was %s", randomText);
+//        
+//        NSString *stringText = [NSString stringWithUTF8String:randomText];
+//
+//        NSString *finalText = [stringText uppercaseString];
+//        
+//        NSLog(@"The louder version of your random text is %@", finalText);
+        
+        // Make it lowercase:
+        
+//        char upperText[225];
+//        
+//        NSLog(@"Enter uppercased texts.");
+//        
+//        fgets(upperText, 225, stdin);
+//        
+//        NSLog(@"The whatever UPPERcased texts you entered just now were %s", upperText);
+//        
+//        NSString *stringUpperTexts = [NSString stringWithUTF8String:upperText];
+//        
+//        NSString *finalUpperTexts = [stringUpperTexts lowercaseString];
+//        
+//        NSLog(@"The lowercased texts are %@", finalUpperTexts);
+//
+        
+        // Numberizing the string:
+        
+//        char randomNumber[250];
+//        
+//        NSLog(@"Enter number!");
+//        
+//        fgets(randomNumber, 250, stdin);
+//        
+//        NSLog(@"The number you entered is %s", randomNumber);
+//        
+//        NSString *stringNumber = [NSString stringWithUTF8String:randomNumber];
+//        
+//        NSString *trimmedNumber = [stringNumber stringByTrimmingCharactersInSet:
+//                           [NSCharacterSet whitespaceCharacterSet]];
+//        
+//        NSInteger finalNumber = [trimmedNumber intValue];
+//        
+//        NSLog(@"The integer is %ld", (long)finalNumber);
+        
+        // Canadianize!
+        
+//        char uncanadianized[225];
+//        
+//        NSLog(@"Enter uncanadianized text!");
+//        
+//        fgets(uncanadianized, 225, stdin);
+//        
+//        NSLog(@"The uncanadianized text you entered was %s", uncanadianized);
+//        
+//        NSMutableString *mutableCanadian = [NSMutableString stringWithUTF8String:uncanadianized];
+//
+//        [mutableCanadian appendString:@" eh?"];
+//        
+//        NSLog(@"The very Canadian version should be %@", mutableCanadian);
+        
+        // Respond
+        
+//        char respond[225];
+//        
+//        NSLog(@"Enter text!");
+//        
+//        fgets(respond, 225, stdin);
+//        
+//        NSLog(@"you entered %s", respond);
+//        
+//        NSString *stringResponse = [[NSString stringWithUTF8String:respond] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+//        
+//        if ([stringResponse hasSuffix:@"?"]) {
+//        
+//            NSLog(@"I do not know.");
+//        
+//        } else if ([stringResponse hasSuffix:@"!"]) {
+//        
+//        
+//            NSLog(@"Hey dede, calm down!");
+//        
+//        }
+//        
+        // De-space-it
+        
+//        char text[225];
+//        
+//        NSLog(@"enter txt!");
+//        
+//        fgets(text, 225, stdin);
+//        
+//        NSLog(@"you entered %s", text);
+//        
+//        NSString *spaceText = [[NSString stringWithUTF8String:text]stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+//        
+//        NSString *final = [spaceText stringByReplacingOccurrencesOfString:@" "
+//    withString:@"-"];
+//        
+//        NSLog(@"%@", final);
+//        
+
     }
+    
     return 0;
 }
